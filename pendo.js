068050f075a09d02f1eec2f8ac4c6208af8d1810
jdkitche <39190900+jdkitche@users.noreply.github.com>
2021-05-10 09:47:58 -0400

    Update pendo.js

--- pendo.js
+++ pendo.js
@@ -85,17 +85,17 @@
 //
 // Segment Install
 
-  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
-  analytics.load("YwftWfBNKY9u0JVe3N1yG9yux5RdCggT");
-  analytics.page();
-  }}();
+//   !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
+//   analytics.load("YwftWfBNKY9u0JVe3N1yG9yux5RdCggT");
+//   analytics.page();
+//   }}();
 
 
 
-analytics.identify('f4ca124298', {
-  name: 'Michael Bolton',
-  email: 'mbolton@initech.com'
-});
+// analytics.identify('f4ca124298', {
+//   name: 'Michael Bolton',
+//   email: 'mbolton@initech.com'
+// });
 
   
 //  <!-- Other Scripts-->
@@ -172,3 +172,7 @@ analytics.identify('f4ca124298', {
 //         });
 //       })('fd2507d5-0da6-47c2-5189-f087c06e6922');
 // </script>
+
+<!-- Start of pendo Zendesk Widget script -->
+<script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=f0fac6b4-cd6f-4dc4-a20e-3becb43e4c8a"> </script>
+<!-- End of pendo Zendesk Widget script -->
